a42e9cd0a9c430713483e7a4c0dfb6a1480e0761
Qenick <kamil.koenig@gmail.com>
2022-07-06 19:23:57 +0200

    fiszki added

---
+++ flashcards/flashcards-js.js
@@ -0,0 +1,29 @@
+//Prosta instrukcja warunkowa - jeśli age jest większe lub równe 18 to wydrukuj 'Jesteś pełnoletni', jeśli nie to
+// wydrukuj 'Jesteś nieletni'.
+
+if (age >= 18) {
+    console.log('Jesteś pełnoletni');
+} else {
+    console.log('Jesteś nieletni');
+}
+
+
+
+
+//Ternary operator w zmiennej age sprawdź czu user jest pełnoletni. Jeśli tak to zmienna ma wartość string "Dorosły"
+// a jak nie to Dziecko.
+
+const age = user >= 18 ? 'Dorosły' : 'Dziecko'
+
+
+
+
+//napisz wzór na losowanie liczby z zakresu min-max
+
+Math.floor(Math.random() * (max - min + 1) + min);
+
+
+// DONE
+// DONE
+// DONE
+// DONE
