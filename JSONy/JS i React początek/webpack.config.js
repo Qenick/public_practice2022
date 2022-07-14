9c144a922f16eda0c7728cb4773fcc7e505fd282
Qenick <kamil.koenig@gmail.com>
2022-07-14 17:25:52 +0200

    To do - fiszki

---
+++ JSONy/JS i React początek/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const entryPath = "03_Dzien_2/05_Export_i_import/01_Zadanie_1";
+
+module.exports = {
+    entry: `./${entryPath}/js/app.js`,
+    output: {
+        filename: "out.js",
+        path: path.resolve(__dirname, `${entryPath}/build`)
+    },
+    devServer: {
+        contentBase: path.join(__dirname, `${entryPath}`),
+        publicPath: "/build/",
+        compress: true,
+        port: 3001
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    }
+};
+
